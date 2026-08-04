5080a66007e1710e7ff7481ac8ca044ddd9f6ae7
Nada ali <elghoulnada17@gmail.com>
2026-08-04 17:36:18 +0300

    /api

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,4 +1,6 @@
-const serverless = require('serverless-http');
-const app = require('../server');
+const serverless = require("serverless-http");
+const app = require("../server");
 
-module.exports = serverless(app);
+module.exports = serverless(app, {
+  basePath: "/api",
+});
